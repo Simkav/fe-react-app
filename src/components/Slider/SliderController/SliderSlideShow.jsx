9cf079776b133993d5417de70ea3e92b55cc32e6
Ivan Khomenko <Simkav2411@gmail.com>
2021-02-21 12:09:54 +0200

    Refactor Styles

--- src/components/Slider/SliderController/SliderSlideShow.jsx
+++ src/components/Slider/SliderController/SliderSlideShow.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import styles from '../Slider.module.scss';
 
 class SliderSlideShow extends Component {
   render() {
     const { switchSlideShow, isSlideShow } = this.props;
     return (
-      <div>
+      <div className={styles.slideShowContainer}>
         <button onClick={switchSlideShow}>SlideShow</button>
         <span>{isSlideShow ? 'Enabled' : 'Disabled'}</span>
       </div>
